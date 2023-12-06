65237585439d6b3cb391fb26f4d13e96aefbedf1
Sunny Hundal <amandip.h@gmail.com>
2023-12-06 11:10:57 -0800

    working chat sender to receiver

--- vite-express-project/src/client/components/ChatModal.jsx
+++ vite-express-project/src/client/components/ChatModal.jsx
@@ -1,15 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useTheme } from "../hooks/ThemeContext";
 import { createClient } from "@supabase/supabase-js";
 import { useAuth } from "../hooks/AuthContext";
-import { supabase } from "./supabase"
-
-
+import { supabase } from "./supabase";
 
 const ChatModal = (closeModal2) => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [showModal, setShowModal] = useState(true);
+  const [lastSender, setLastSender] = useState(null);
+  const {notifications, setNotifications} = useTheme([]);
+  let holder = 0;
+  const messagesEndRef = useRef(null);
 
   const closeModal = () => {
     setShowModal(false);
@@ -26,7 +28,9 @@ const ChatModal = (closeModal2) => {
         { event: "*", schema: "public", table: "messages" },
         (payload) => {
           setMessages((prevMessages) => [...prevMessages, payload.new]);
-          console.log("Change received!", payload);
+          holder = payload.new.sender_id
+          console.log("Holder", holder);
+          console.log("Change received!", payload.new.sender_id);
         }
       )
       .subscribe();
@@ -37,8 +41,7 @@ const ChatModal = (closeModal2) => {
     };
   }, []);
 
- 
- // Fetch old messages for presistent chat
+  // Fetch old messages for presistent chat
   useEffect(() => {
     const fetchMessages = async () => {
       try {
@@ -62,11 +65,26 @@ const ChatModal = (closeModal2) => {
   const sendMessage = async () => {
     try {
       if (newMessage.trim() !== "") {
-        const timestamp = new Date().toISOString(); 
+        const timestamp = new Date().toISOString();
+
+        const { data: lastMessageData, error: lastMessageError } = await supabase
+        .from("messages")
+        .select("sender_id")
+        .order("timestamp", { ascending: false })
+        .limit(1);
+
+      if (lastMessageError) {
+        throw lastMessageError;
+      }
+
+      const receiver_id = (lastMessageData && lastMessageData.length > 0)
+        ? lastMessageData[0].sender_id
+        : "default_receiver_id"; 
+
         const { data, error } = await supabase.from("messages").insert([
           {
-            sender_id: `${loggedInUser.id}`, 
-            receiver_id: "2", 
+            sender_id: `${loggedInUser.id}`,
+            receiver_id,
             text: newMessage,
             timestamp,
             channel: "YOUR_CHANNEL_NAME",
@@ -76,43 +94,52 @@ const ChatModal = (closeModal2) => {
           throw error;
         }
         setNewMessage("");
+        setLastSender(loggedInUser.id);
+        console.log("LS", lastSender);
       }
     } catch (error) {
       console.error("Error sending message:", error.message);
     }
   };
 
-  console.log("loggedInUser", loggedInUser);
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   return (
     <div>
       <div className="fixed top-1/2 right-6 transform -translate-x-1/2 -translate-y-1/2 w-80 h-96 border rounded-lg overflow-hidden">
         <div className="bg-white p-4 rounded-lg shadow-lg h-full flex flex-col">
-          <h1 className="fixed top-1 right-6">
+          <h1 className="fixed top-1 right-6" onClick={closeModal}>
             X
           </h1>
           <p>{messages.channel}</p>
           <ul className="flex-1 overflow-y-auto mb-4">
             {Object.values(messages).map((msg, index) => {
-              const isSentByLoggedInUser = msg.sender_id == loggedInUser.id;
+              const isSentByLoggedInUser =
+                msg.sender_id == loggedInUser.id ||
+                msg.receiver_id == loggedInUser.id;
 
-              return (
-                <li
-                  key={index}
-                  className={`mb-2 ${
-                    isSentByLoggedInUser ? "chat chat-end" : "chat chat-start"
-                  }`}
-                >
-                  <p className="chat-bubble">{msg.text}</p>
-                  <div className="chat-header">
-                    <strong>
-                      {isSentByLoggedInUser ? "You" : msg.sender_id}
-                    </strong>
-                    :
-                  </div>
-                </li>
-              );
+              if (isSentByLoggedInUser) {
+                const isSentByUser = msg.sender_id == loggedInUser.id;
+
+                return (
+                  <li
+                    key={index}
+                    className={`mb-2 ${
+                      isSentByUser ? "chat chat-end" : "chat chat-start"
+                    }`}
+                  >
+                    <p className="chat-bubble">{msg.text}</p>
+                    <div className="chat-header">
+                      <strong>{isSentByUser ? "You" : msg.sender_id}</strong> :
+                    </div>
+                  </li>
+                );
+              }
+              return null; // To skip rendering messages not related to the logged-in user
             })}
+            <div ref={messagesEndRef} />
           </ul>
           <input
             type="text"
--- vite-express-project/src/client/components/supabase.js
+++ vite-express-project/src/client/components/supabase.js
@@ -1,11 +1,14 @@
 import { createClient } from "@supabase/supabase-js";
 
-require("dotenv").config({
-  path: "./.env.local",
-});
+// require("dotenv").config({
+//   path: "./.env.local",
+// });
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_KEY;
+const VITE_SUPABASE_URL="https://iwxsgpjsqhbnsvoepczn.supabase.co"
+const VITE_SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3eHNncGpzcWhibnN2b2VwY3puIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAwMTEzNDYsImV4cCI6MjAxNTU4NzM0Nn0.fpfSwbGJgXpvTnkYkBFloBRH_sn1vkAXJC2OI5pAgt4"
+
+const supabaseUrl = VITE_SUPABASE_URL;
+const supabaseKey = VITE_SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
